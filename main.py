e12bbd93899655094010ac949a3e9b6bc7b45e0d
Jianghe <caojianghe@hotmail.com>
2024-01-28 21:21:50 +0800

    Supported conversation

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 import os
 
-from openai import OpenAI
+from openai import OpenAI, Stream
+from openai.types.chat import ChatCompletionChunk
 
-openai_client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 
+class ChatBot:
+    def __init__(self, model: str = "gpt-4"):
+        self.messages = []
+        self.openai_client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+        self.model = model
 
-def chat_gpt(prompt, model="gpt-4"):
-    try:
-        stream = openai_client.chat.completions.create(
-            model=model,
-            messages=[{"role": "user",
-                       "content": prompt}],
-            stream=True,
-        )
-        return stream
-    except Exception as e:
-        return str(e)
+    def chat(self, message: str) -> Stream[ChatCompletionChunk] | str:
+        try:
+            self.messages.append({'role': 'user', 'content': message})
+            stream = self.openai_client.chat.completions.create(
+                model=self.model,
+                messages=self.messages,
+                stream=True,
+            )
+            return stream
+        except Exception as e:
+            str(e)
 
 
 def main():
-    print("Welcome to ChatGPT! Type 'quit' to exit.")
+    print("Welcome to ChatGPT! Type 'bye' to exit.\n")
+    chatbot = ChatBot()
 
     while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'quit':
+        # Get user input
+        user_input = input("You:\n")
+        print()
+        if user_input.lower() == 'bye':
             break
-
-        stream = chat_gpt(user_input)
-        print("ChatGPT: ", end="")
+        # Get response from OpenAI
+        print("ChatGPT:")
+        stream = chatbot.chat(user_input)
+        if type(stream) is str:
+            print(stream)
+            continue
         for chunk in stream:
             print(chunk.choices[0].delta.content or "", end="")
-        print()
+        print("\n")
 
 
 if __name__ == "__main__":
